ec03b6135d3b3bb645099d5303c2169b93446f79
Tomas Harkema <tomas@harkema.io>
2024-02-04 23:16:31 +0100

    temporary just log to dev null

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ var (
 )
 
 func createLogger() {
-	logPath := "/tmp/nixos-devices-server.log"
+	logPath :=  os.DevNull // "/tmp/nixos-devices-server.log"
 	lf, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
 	if err != nil {
 		logger.Fatalf("Failed to open log file: %v", err)
